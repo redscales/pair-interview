94689e2607beeb0c122682510d5c38869edda983
Dheeraj Erraballi <dj.erraballi@gmail.com>
2026-04-27 12:59:50 -0500

    feat(frontend): recipe detail page with delete

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import "./index.css";
 import RootLayout from "./routes/root-layout";
 import RecipesListPage from "./routes/recipes-list";
+import RecipeDetailPage from "./routes/recipe-detail";
 import IngredientsListPage from "./routes/ingredients-list";
 
 const router = createBrowserRouter([
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Navigate to="/recipes" replace /> },
       { path: "recipes", Component: RecipesListPage },
+      { path: "recipes/:id", Component: RecipeDetailPage },
       { path: "ingredients", Component: IngredientsListPage },
     ],
   },
